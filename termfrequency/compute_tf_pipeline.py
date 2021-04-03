3e0473464f0f62e6ec298de2ad9648256d2160b2
leflocha <leflocha@allegheny.edu>
2021-04-03 16:02:03 -0400

    test

--- termfrequency/compute_tf_pipeline.py
+++ termfrequency/compute_tf_pipeline.py
@@ -1,9 +1,10 @@
-# TODO: Refactor all of this source code so that it fully adheres
-# to the source code standard for the Python programming language
-
 #!/usr/bin/env python
 # pylint: disable=C0114 # missing-module-docstring
-import sys, re, operator, string
+import sys
+import re
+import operator
+import string
+
 
 #
 # The functions
@@ -17,6 +18,7 @@ def read_file(path_to_file):
         data = f.read()
     return data
 
+
 def filter_chars_and_normalize(str_data):
     """
     Takes a string and returns a copy with all non-alphanumeric
@@ -25,6 +27,7 @@ def filter_chars_and_normalize(str_data):
     pattern = re.compile(r'[\W_]+')
     return pattern.sub(' ', str_data).lower()
 
+
 def scan(str_data):
     """
     Takes a string and scans for words, returning
@@ -32,16 +35,18 @@ def scan(str_data):
     """
     return str_data.split()
 
+
 def remove_stop_words(word_list):
     """
     Takes a list of words and returns a copy with all stop
     words removed
     """
-    with open('../stop_words.txt') as f:
+    with open('stopwords/stop_words.txt') as f:
         stop_words = f.read().split(',')
     # add single-letter words
     stop_words.extend(list(string.ascii_lowercase))
-    return [w for w in word_list if not w in stop_words]
+    return [w for w in word_list if w not in stop_words]
+
 
 def frequencies(word_list):
     """
@@ -56,6 +61,7 @@ def frequencies(word_list):
             word_freqs[w] = 1
     return word_freqs
 
+
 def sort(word_freq):
     """
     Takes a dictionary of words and their frequencies
@@ -64,19 +70,24 @@ def sort(word_freq):
     """
     return sorted(word_freq.items(), key=operator.itemgetter(1), reverse=True)
 
+
 def print_all(word_freqs):
     """
     Takes a list of pairs where the entries are sorted by frequency and print them recursively.
     """
-    if(len(word_freqs) > 0):
+    if len(word_freqs) > 0:
         print(word_freqs[0][0], ' - ', word_freqs[0][1])
-        print_all(word_freqs[1:]);
+        print_all(word_freqs[1:])
+
 
 #
 # The main function
 #
 
-if __name__ == "__main__":
+def main():
+    """
+    The main function, runs all other functions
+    """
     str_data = read_file(sys.argv[1])
     str_data = filter_chars_and_normalize(str_data)
     word_list = scan(str_data)
@@ -86,3 +97,16 @@ if __name__ == "__main__":
 
     for tf in word_freqs[0:25]:
         print(tf[0], "-", tf[1])
+
+
+if __name__ == "__main__":
+    # string_data = read_file(sys.argv[1])
+    # string_data = filter_chars_and_normalize(string_data)
+    # words_list = scan(string_data)
+    # words_list = remove_stop_words(words_list)
+    # words_freq = frequencies(words_list)
+    # words_freqs = sort(words_freq)
+
+    # for tf in words_freqs[0:25]:
+    #     print(tf[0], "-", tf[1])
+    main()
